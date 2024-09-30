9750fd811196cd1ea70d741da163b9034bfc2f5c
kuryakov.dv <denis.kuryakov@sbermarket.ru>
2024-09-30 14:51:28 +0300

    MARKETPLACE-1 Реализация хука useVirtualScroll

--- src/shared/hooks/useVirtualScroll.ts
+++ src/shared/hooks/useVirtualScroll.ts
@@ -1,6 +1,28 @@
+import { useCallback, useState } from 'react'
+
+export type ScrollDirection = 'forward' | 'backward';
+
+export type ScrollState = {
+  scrollOffset: number;
+  scrollDirection: ScrollDirection;
+}
 
 function useVirtualScroll() {
+  const initValue: ScrollState = { scrollOffset: 0, scrollDirection: 'forward' };
+  const [scrollState, setScrollState] = useState<ScrollState>(initValue);
 
+  const onScroll = useCallback((clientExtent: number, scrollExtent: number, scrollOffset: number) => {
+    if (scrollState.scrollOffset === scrollOffset) {
+      return;
+    }
+
+    const newOffset = Math.max(0, Math.min(scrollOffset, scrollExtent - clientExtent));
+    const newScrollDirection = scrollState.scrollOffset <= newOffset ? 'forward' : 'backward';
+
+    setScrollState({ scrollOffset: newOffset, scrollDirection: newScrollDirection });
+  }, [scrollState]);
+
+  return [scrollState, onScroll] as const;
 }
 
 export default useVirtualScroll;
