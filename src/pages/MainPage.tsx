7e3500ed394b1b32114b7fc20bc87f22eac267c9
kuryakov.dv <denis.kuryakov@sbermarket.ru>
2024-10-26 18:12:31 +0300

    MARKETPLACE-3 Использование компонента ProductList

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,8 @@
 import ProductList from '../features/productList/components/ProductList/ProductList.tsx'
 import { MOCK_PRODUCTS } from '../features/productList/mocks'
-import ProductVirtualList from '../features/productList/components/ProductVirtualList/ProductVirtualList.tsx'
 
 function MainPage() {
-  return <ProductVirtualList products={MOCK_PRODUCTS} />
+  return <ProductList products={MOCK_PRODUCTS} />
 }
 
 export default MainPage
