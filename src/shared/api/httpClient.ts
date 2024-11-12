5a05732aa2fee58ddaf3b5a6b0d27e14404272cb
kuryakov.dv <denis.kuryakov@sbermarket.ru>
2024-11-12 08:18:47 +0300

    MARKETPLACE-3 Создание env-файла

--- src/shared/api/httpClient.ts
+++ src/shared/api/httpClient.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8888'
 const TIMEOUT = 5000
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: TIMEOUT,
 });
 
