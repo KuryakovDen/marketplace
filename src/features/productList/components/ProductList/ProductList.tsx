abe7d5b088496b7628863b3244f7d82f64be64e8
kuryakov.dv <denis.kuryakov@sbermarket.ru>
2024-11-17 11:58:11 +0300

    MARKETPLACE-6 Рефакторинг

--- src/features/productList/components/ProductList/ProductList.tsx
+++ src/features/productList/components/ProductList/ProductList.tsx
@@ -1,13 +1,12 @@
 import ProductItem from '../ProductItem/ProductItem.tsx'
 import styles from './ProductList.module.css'
 import useGetProductList from '../../hooks/useGetProductList.ts'
-import { Product } from '../../../../shared/types/product/Product.types.ts'
 import useIntersectionObserver from '../../../../shared/hooks/useIntersectionObserver.ts'
 import { MutableRefObject, useCallback } from 'react'
 
 function ProductList() {
   const {
-    data,
+    data = {},
     fetchNextPage: fetchNextProducts,
     hasNextPage,
     isLoading: isProductListLoading,
@@ -19,6 +18,8 @@ function ProductList() {
     enabled: hasNextPage && !isFetchingNextPage,
   });
 
+  const { preparedProducts: products } = data
+
   const lastProductRef = useCallback((node: HTMLLIElement | null) => {
     if (node !== null) {
       (intersectionRef as MutableRefObject<HTMLElement | null>).current = node;
@@ -28,8 +29,6 @@ function ProductList() {
   if (isProductListLoading) {
     return // TODO Заменить на спиннер/скелетон
   }
-
-  const products: Product[] = data?.pages.flatMap(page => page.products) || [];
 
   return (
     <ul className={styles.productList}>
--- src/features/productList/hooks/useGetProductList.ts
+++ src/features/productList/hooks/useGetProductList.ts
@@ -1,21 +1,25 @@
 import ApiService from '../../../shared/api/apiService.ts'
 import { Product } from '../../../shared/types/product/Product.types.ts'
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { InfiniteData, useInfiniteQuery, UseInfiniteQueryResult } from '@tanstack/react-query'
 
-interface ApiResponse {
+interface ProductListApiResponse {
   products: Product[];
   page: number;
   total: number;
 }
 
-const useGetProductList = () => {
+interface InfiniteProductData extends InfiniteData<ProductListApiResponse> {
+  preparedProducts: Product[];
+}
+
+const useGetProductList = (): UseInfiniteQueryResult<InfiniteProductData, Error> => {
   const PRODUCT_LIST_KEY = 'productList';
   const DEFAULT_PRODUCT_LIMIT = 10;
 
-  return useInfiniteQuery<ApiResponse, Error>({
+  return useInfiniteQuery<ProductListApiResponse, Error, InfiniteProductData>({
     queryKey: [PRODUCT_LIST_KEY],
     queryFn: async ({ pageParam = 1 }) => {
-      return await ApiService.get<ApiResponse>(`products/search?page=${pageParam}&limit=${DEFAULT_PRODUCT_LIMIT}`);
+      return await ApiService.get<ProductListApiResponse>(`products/search?page=${pageParam}&limit=${DEFAULT_PRODUCT_LIMIT}`);
     },
     getNextPageParam: (lastPage, allPages) => {
       const totalProductsLoaded = allPages.reduce((sum, page) => sum + page.products.length, 0);
@@ -23,9 +27,12 @@ const useGetProductList = () => {
 
       return hasNextPage ? lastPage.page + 1 : undefined;
     },
+    select: (data): InfiniteProductData => ({
+      ...data,
+      preparedProducts: data.pages.flatMap(page => page.products)
+    }),
     initialPageParam: 1,
-    enabled: true,
   });
 };
 
-export default useGetProductList
+export default useGetProductList;
