e919fa507cf5c05051ca7badae4c078799c0045a
kuryakov.dv <denis.kuryakov@sbermarket.ru>
2024-11-17 07:51:06 +0300

    MARKETPLACE-6 Использование хука useIntersectionObserver

--- src/features/productList/components/ProductList/ProductList.tsx
+++ src/features/productList/components/ProductList/ProductList.tsx
@@ -2,65 +2,55 @@ import ProductItem from '../ProductItem/ProductItem.tsx'
 import styles from './ProductList.module.css'
 import useGetProductList from '../../hooks/useGetProductList.ts'
 import { Product } from '../../../../shared/types/product/Product.types.ts'
-import { useCallback, useEffect, useRef } from 'react'
+import useIntersectionObserver from '../../../../shared/hooks/useIntersectionObserver.ts'
+import { MutableRefObject, useCallback } from 'react'
 
 function ProductList() {
   const {
     data,
-    fetchNextPage,
+    fetchNextPage: fetchNextProducts,
     hasNextPage,
     isLoading: isProductListLoading,
     isFetchingNextPage
   } = useGetProductList();
 
-  const observerTarget = useRef<HTMLDivElement>(null as unknown as HTMLDivElement);
+  const intersectionRef = useIntersectionObserver({
+    onIntersect: () => {
+      if (hasNextPage && !isFetchingNextPage) {
+        void fetchNextProducts();
+      }
+    },
+    enabled: hasNextPage && !isFetchingNextPage,
+  });
 
-  const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
-    const [target] = entries;
-    if (target.isIntersecting && hasNextPage && !isFetchingNextPage) {
-      fetchNextPage();
+  const lastProductRef = useCallback((node: HTMLLIElement | null) => {
+    if (node !== null) {
+      (intersectionRef as MutableRefObject<HTMLElement | null>).current = node;
     }
-  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
-
-  useEffect(() => {
-    const element = observerTarget.current;
-    if (!element) return;
-
-    const option = {
-      threshold: 0.1
-    };
-
-    const observer = new IntersectionObserver(handleObserver, option);
-    observer.observe(element);
-
-    return () => observer.unobserve(element);
-  }, [handleObserver]);
+  }, [intersectionRef]);
 
   if (isProductListLoading) {
-    return <div>Loading...</div>; // TODO: Заменить на спиннер/скелетон
+    return // TODO: Заменить на спиннер/скелетон
   }
 
   const products: Product[] = data?.pages.flatMap(page => page.products) || [];
 
   return (
-    <>
-      <ul className={styles.productList}>
-        {products.map((product) =>
-          <li key={product.id} className={styles.productItem}>
-            <ProductItem
-              {...product}
-              onProductClick={() => {}}
-              onFavouriteClick={() => {}}
-            />
-          </li>
-        )}
-      </ul>
-      {hasNextPage && (
-        <div ref={observerTarget} style={{ height: '20px', margin: '20px 0' }}>
-          {isFetchingNextPage ? 'Loading more...' : ''}
-        </div>
-      )}
-    </>
+    <ul className={styles.productList}>
+      {products.map((product, index) => (
+        <li
+          key={product.id}
+          className={styles.productItem}
+          ref={index === products.length - 1 ? lastProductRef : undefined}
+        >
+          <ProductItem
+            {...product}
+            onProductClick={() => {}}
+            onFavouriteClick={() => {}}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
 
