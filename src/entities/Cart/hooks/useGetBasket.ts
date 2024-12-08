4daa6b7fe21e1b83e6789d77f152d315e1e2d8cf
kuryakov.dv <denis.kuryakov@sbermarket.ru>
2024-12-08 06:47:17 +0300

    MARKETPLACE-10 Добавление сущностей для корзины

---
+++ src/entities/Cart/hooks/useGetBasket.ts
@@ -0,0 +1,14 @@
+
+function useGetBasket() {
+  // Знает с каким стором взаимодействуем
+  // Знает какое конкретно используем браузерное хранилище
+  // Знает
+
+  // const basketManger = new BasketManager();
+
+  // return {
+  //     basketManager
+  // }
+}
+
+export default useGetBasket;
